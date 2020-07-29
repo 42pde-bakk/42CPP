cac728df5ead886a395d3922eb3772a4ddbc8118
Peer De bakker <pde-bakk@f1r4s18.codam.nl>
2020-07-29 16:48:27 +0200

    formatting

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,18 +6,18 @@
 /*   By: Peer <pde-bakk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/25 15:35:48 by pde-bakk      #+#    #+#                 */
-/*   Updated: 2020/07/28 09:40:46 by Peer          ########   odam.nl         */
+/*   Updated: 2020/07/29 15:29:37 by pde-bakk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
   if (argc == 1)
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
   for (int i = 1; argv[i]; i++)
     for (int n = 0; argv[i][n]; n++)
       std::cout << (char)toupper(argv[i][n]);
   std::cout << std::endl;
-  return 0;
+  return (0);
 }
