60c86838cf75c4971c2c0bd5704752e4c9298c8e
Peer De bakker <pde-bakk@f0r1s12.codam.nl>
2020-08-03 18:05:05 +0200

    idk what I changed for this commit

--- module01/ex05/Brain.cpp
+++ module01/ex05/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: Peer <pde-bakk@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/07/28 10:09:22 by Peer          #+#    #+#                 */
-/*   Updated: 2020/07/28 10:39:32 by Peer          ########   odam.nl         */
+/*   Updated: 2020/08/03 11:44:43 by pde-bakk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 Brain::Brain() {
 	_amountOfBraincells = 2;
 	_smarts = 0;
- }
+}
 
 const Brain*	Brain::identify() const {
 	return this;
