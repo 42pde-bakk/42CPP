71b3c352278b78dcfab6ce199d21397016b3dc15
Peer De bakker <pde-bakk@f1r4s18.codam.nl>
2020-08-14 23:48:55 +0200

    yaas queen

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: peer <peer@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/12 17:45:33 by peer          #+#    #+#                 */
-/*   Updated: 2020/08/12 18:17:27 by peer          ########   odam.nl         */
+/*   Updated: 2020/08/14 20:02:19 by pde-bakk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 # define EASYFIND_HPP
 #include <algorithm>
 
-template< typename T>
+template <typename T>
 typename T::iterator easyfind(T& container, const int& tofind) {
-	return std::find(container.begin(), container.end(), tofind);
+	typename T::iterator found;
+
+	found = std::find(container.begin(), container.end(), tofind);
+	if (found == container.end())
+		throw std::runtime_error("Element not found");
+	return found;
 }
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: peer <peer@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/12 18:07:13 by peer          #+#    #+#                 */
-/*   Updated: 2020/08/12 18:15:08 by peer          ########   odam.nl         */
+/*   Updated: 2020/08/14 20:02:25 by pde-bakk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,45 @@
 #include <vector>
 #include <list>
 
-int	main(void) {
-	std::vector<int>	vec;
+void	vectors(void) {
+	std::vector<int>	PeerVector;
 	srand(time(NULL));
 	int tofind = rand() % 100;
+	std::cout << "int tofind is " << tofind << std::endl;
 	for (int i = 0; i < 10; i++)
-		vec.push_back(rand() % 100);
-	vec.push_back(tofind);
+		PeerVector.push_back(rand() % 100);
+	PeerVector.push_back(tofind);
 	for (int i = 0; i < 20; i++)
-		vec.push_back(rand() % 100);
+		PeerVector.push_back(rand() % 100);
+	try {
+		std::vector<int>::iterator found = easyfind(PeerVector, tofind);
+		std::cout << "Element found: " << *found << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+}
 
-	std::vector<int>::iterator found = easyfind(vec, tofind);
-	if (found != vec.end())
-		std::cout << "found: " << *found << std::endl;
-	else
-		std::cout << "not found... :(" << std::endl;
+void	lists(void) {
+	std::list<int>	PeerList;
+	srand(time(NULL));
+	int tofind = rand() % 100;
+	std::cout << "int tofind is " << tofind << std::endl;
+	for (int i = 0; i < 10; i++)
+		PeerList.push_back(rand() % 100);
+	PeerList.push_back(tofind);
+	for (int i = 0; i < 20; i++)
+		PeerList.push_back(rand() % 100);
+	try {
+		std::list<int>::iterator found = easyfind(PeerList, tofind);
+		std::cout << "Element found: " << *found << std::endl;
+	}
+	catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+}
+
+int	main(void) {
+	vectors();
+	lists();
 }
