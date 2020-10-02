7b2dab5574eb129cdba5c01acc71719efe72f3e0
Peer de Bakker <pde-bakk@student.codam.nl>
2020-10-02 13:21:20 +0200

    simplified Coplien functions

--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: pde-bakk <pde-bakk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/15 00:07:51 by pde-bakk      #+#    #+#                 */
-/*   Updated: 2020/08/16 22:24:24 by peer          ########   odam.nl         */
+/*   Updated: 2020/10/02 13:20:35 by peerdb        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,16 @@
 template < class T, class Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 public:
-	MutantStack();
-	~MutantStack();
-	MutantStack(const MutantStack<T, Container> &other);
-	MutantStack<T, Container>&	operator=(const MutantStack<T, Container> &other);
+	MutantStack() : std::stack<T,Container>() {}
+	~MutantStack() {}
+	MutantStack(const MutantStack<T, Container> &other) : std::stack<T,Container>() {
+		*this = other;
+	}
+	MutantStack&	operator=(const MutantStack& x) {
+		if (this != &x)
+			this->c = x.c;
+		return *this;
+	}
 
 	typedef typename std::stack<T, Container>::container_type::iterator iterator;
 	typedef typename std::stack<T, Container>::container_type::const_iterator const_iterator;
@@ -57,23 +63,4 @@ public:
 	}
 };
 
-template<class T, class Container>
-MutantStack<T, Container>::MutantStack() : std::stack<T, Container>() { }
-
-template<class T, class Container>
-MutantStack<T, Container>::~MutantStack() {}
-
-template<class T, class Container>
-MutantStack<T, Container>::MutantStack(const MutantStack<T, Container> &other) 
-	: std::stack<T, Container>(other) {	
-}
-
-template<class T, class Container>
-MutantStack<T, Container>&
-MutantStack<T, Container>::operator=(const MutantStack<T, Container> &other) {
-	if (this != &other) {
-		this->c = other.c;
-	}
-	return *this;	
-}
 #endif
